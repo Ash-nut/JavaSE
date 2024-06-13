339cf4b8442f37f4e9ca4859b614c7731517fc15
Xinrui Yi <1327803942@qq.com>
2024-06-13 14:35:46 +0800

    Update Exercises2.java

--- HelloWorld/src/cn/helloworld1999/Exercises2.java
+++ HelloWorld/src/cn/helloworld1999/Exercises2.java
@@ -4,34 +4,30 @@ import java.util.Scanner;
 
 public class Exercises2 {
     public static void main(String[] args) {
+        // exercise1();
         exercise2();
+
     }
 
-    public static void exercise1() {
-        System.out.println("输入一个正整数，以判断是否为质数：");
-        Scanner sc = new Scanner(System.in);
-        int temp = sc.nextInt();
+    public static boolean exercise1(int temp) {
+        if (temp == 1) {
+            return false;
+        }
         for (int i = temp - 1; i >= 1; i--) {
             if ((temp % i == 0) && (i != 1)) {
-                System.out.println(temp + "不是一个质数");
-                return;
+                return false;
             }
         }
-        System.out.println(temp + "是一个质数");
+        return true;
     }
 
     public static void exercise2() {
-        // 从100倒数
-        for (int loop = 1; loop <= 100; loop++) {
-            // 判断每一个数字是否是质数
-            boolean flag = false;
-            for (int i = loop; i >= 0; i--) {
-                if ((loop % i == 0) && (i != 1)) {
-                    System.out.println(i + "不是质数");
-                    break;
-                }
+        for (int loop = 1; loop < 101; loop++) {
+            if (exercise1(loop)) {
+                System.out.println(loop);
+            } else {
+                continue;
             }
-            System.out.println(loop);
         }
     }
 }
