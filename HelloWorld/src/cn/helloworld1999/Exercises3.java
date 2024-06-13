693ea1391eb00b3c7c9566ab6288141744d811a2
Xinrui Yi <1327803942@qq.com>
2024-06-13 19:11:32 +0800

    写完作业了

---
+++ HelloWorld/src/cn/helloworld1999/Exercises3.java
@@ -0,0 +1,67 @@
+package cn.helloworld1999;
+
+public class Exercises3 {
+    public static void main(String[] args) {
+        exercise4();
+    }
+
+    static void exercise1() {
+        int sum = 0;
+        int arr[] = new int[100];
+        for (int i = 0; i <= arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.println(arr[i]);
+        }
+        for (int i = 0; i <= 99; i++) {
+            if (arr[i] % 2 == 0) {
+                sum += arr[i];
+            } else {
+                continue;
+            }
+        }
+        System.out.println("sum= " + sum);
+    }
+
+    static void exercise2() {
+        int arr[] = { 23, 45, 673, 76, 2, 87, 1 };
+        int min = arr[0];
+        for (int i : arr) {
+            min = i > min ? min : i;
+        }
+        System.out.println("min = " + min);
+    }
+
+    // 似乎写错了，但是错在哪了呢？
+    static void exercise3() {
+        int arr[] = { 23, 45, 673, 76, 2, 87, 1 };
+        boolean bo = false;
+        while (bo) {
+            for (int i = 0; arr[i] > arr[i + 1]; i++) {
+                arr[i] = arr[i + 1];
+                if (i == arr.length) {
+                    bo = true;
+                    for (int a : arr) {
+                        System.out.print(i + " ");
+                    }
+                }
+                i = i >= arr.length - 1 ? 0 : i;
+            }
+        }
+    }
+
+    static void exercise4() {
+        String name[] = new String[] { "倩倩", "刘亦菲", "zsl", "lucy" };
+        String name1[] = new String[name.length + 1];
+        for (int i = 0; i < name.length; i++) {
+            name1[i] = name[i];
+        }
+        name1[name.length] = "小尾巴";
+        name = name1;
+        for (String str : name) {
+            System.out.println(str);
+        }
+    }
+    static void exercise5(){
+        String name[] = new String[] { "倩倩", "刘亦菲", "zsl", "lucy" };
+    }
+} \
---
+++ HelloWorld/src/cn/helloworld1999/HomeWorkd3day.java
@@ -0,0 +1,50 @@
+package cn.helloworld1999;
+
+/**
+ * @author XinruiYi
+ * @version 0.0.1
+ */
+public class HomeWorkd3day {
+    static int[] balls = new int[7]; // 七个球
+
+    public static void main(String[] args) {
+        // 抽红的
+        for (int i = 0; i < 6; i++) {
+            balls[i] = getRedBall(balls);
+        }
+        // 抽蓝的
+        balls[balls.length - 1] = getBlueBall();
+        // 展示结果
+        showBalls(balls);
+    }
+
+    /**
+     * 我用这个函数传出来这次的结果，并且在得到本次结果之前我会和已有的数组进行比较
+     */
+    static int getRedBall(int[] balls) {
+        boolean flag = false;
+        int redBall = 0;
+        redBall = (int) (Math.random() * 33 + 1);
+        // roll到成功为止
+        while (flag) {
+            // 比较本次 roll 出来的是否存在于数组内
+            for (int i = 0; i < 6; i++) {
+                if (redBall != balls[i])
+                    flag = true;
+            }
+        }
+        return redBall;
+    }
+
+    // 不值一提
+    static int getBlueBall() {
+        return (int) (Math.random() * 16 + 1);
+    }
+
+    // 展示球
+    static void showBalls(int[] balls) {
+        for (int ball : balls) {
+            System.err.print(ball + " ");
+        }
+    }
+}
