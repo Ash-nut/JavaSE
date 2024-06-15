359b2be7f6dfd0cd2d74d22cd3fc3f3195fafcfd
Xinrui Yi <1327803942@qq.com>
2024-06-15 14:38:00 +0800

    改好了

--- HelloWorld/src/cn/helloworld1999/InversionArray.java
+++ HelloWorld/src/cn/helloworld1999/InversionArray.java
@@ -3,14 +3,24 @@ package cn.helloworld1999;
 import java.util.Arrays;
 
 /**
- * 晚上写
+ * 数组反转
+ * 改好了
  */
 
 public class InversionArray {
     public static void main(String[] args) {
-
+        demo();
     }
 
     static int[] arr = { 28, 11, 22, 7, 15, 9, 16 };
+
+    static void demo() {
+        for (int i = 0; i <= arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 
 }
