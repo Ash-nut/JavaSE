7a6e1c24cfd12beaed3e5ddbf58ea9a202b1e2fb
Xinrui Yi <1327803942@qq.com>
2024-06-24 12:17:32 +0800

    nothing

---
+++ MyArrayList/src/main/java/cn/helloworld1999/MyArrayList.java
@@ -0,0 +1,60 @@
+package cn.helloworld1999;
+
+import java.util.Arrays;
+
+public class MyArrayList {
+    private Object[] eleArray;
+    private int size = 0;
+
+    public MyArrayList() {
+        eleArray = new Object[10];
+    }
+
+    public void add(int index, Object element) {
+        if (size == eleArray.length) {
+            eleArray = Arrays.copyOf(eleArray, size * 2);
+        }
+        eleArray[index] = element;
+    }
+
+    public void remove(int index) {
+        if (size <= 0 || index >= size || index < 0) {
+            throw new RuntimeException("没东西可删");
+        }
+        Object ele = eleArray[index];
+        System.arraycopy(eleArray, index + 1, eleArray, index, size - index - 1);
+        eleArray[--size] = null;
+    }
+
+    public Object get(int index) {
+        if (size <= 0 || index >= size || index < 0) {
+            throw new RuntimeException("没东西可拿");
+        }
+        return eleArray[index];
+    }
+
+    public void set(int index, Object element) {
+        if (size <= 0 || index >= size || index < 0) {
+            throw new RuntimeException("没东西可改");
+        }
+        eleArray[index] = element;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void insert(int index, Object element) {
+        if (size == eleArray.length) {
+            eleArray = Arrays.copyOf(eleArray, size * 2);
+        }
+        System.arraycopy(eleArray, index, eleArray, index + 1, size - index);
+        eleArray[index] = element;
+        size ++;
+    }
+
+}
